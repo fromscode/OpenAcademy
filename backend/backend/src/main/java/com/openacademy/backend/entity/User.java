0c2cccdf2e596eb5022d75ea8e2a2118e696134a
Shubham Chakraborty <chakrabortyshubham00@gmail.com>
2025-08-03 13:13:13 +0530

    Refactor long to Long for better jpa support

--- backend/backend/src/main/java/com/openacademy/backend/entity/User.java
+++ backend/backend/src/main/java/com/openacademy/backend/entity/User.java
@@ -26,7 +26,7 @@ import lombok.Setter;
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private long id;
+    private Long id;
 
     @Column(nullable = false, unique = true)
     private String email;
