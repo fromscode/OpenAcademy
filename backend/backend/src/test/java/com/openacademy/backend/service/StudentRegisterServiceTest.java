e8e0329b53529b3b1f62297c1a6584579a9ed1da
Shubham Chakraborty <chakrabortyshubham00@gmail.com>
2025-08-06 18:51:11 +0530

    create Unit tests for Student Register Service

---
+++ backend/backend/src/test/java/com/openacademy/backend/service/StudentRegisterServiceTest.java
@@ -0,0 +1,76 @@
+package com.openacademy.backend.service;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.time.LocalDate;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import com.openacademy.backend.dto.StudentRegisterRequest;
+import com.openacademy.backend.entity.Student;
+import com.openacademy.backend.entity.User;
+import com.openacademy.backend.repository.StudentRepository;
+import com.openacademy.backend.service.impls.StudentRegisterServiceImpl;
+
+public class StudentRegisterServiceTest {
+    @InjectMocks
+    StudentRegisterServiceImpl service;
+
+    @Mock UserRegisterService userService;
+
+    @Mock
+    StudentRepository repo;
+
+    private StudentRegisterRequest request;
+
+    @BeforeEach
+    public void initialize() {
+        MockitoAnnotations.openMocks(this);
+        request = new StudentRegisterRequest();
+        request.setEmail("test@example.com");
+        request.setDateOfBirth(LocalDate.of(2022, 12, 18));
+        request.setFirstName("John");
+        request.setLastName(("Doe"));
+        request.setPassword("abababa");
+        request.setPassword("1234567896");
+    }
+
+    @Test
+    public void validStudentRegisteredSuccessfully() {
+        User user = new User();
+        user.setEmail(request.getEmail());
+
+        when(userService.registerUser(any(StudentRegisterRequest.class))).thenReturn(user);
+
+        String result = service.registerStudent(request);
+        ArgumentCaptor<Student> studentCaptor = ArgumentCaptor.forClass(Student.class);
+
+        verify(repo).save(studentCaptor.capture());
+        Student savedStudent = studentCaptor.getValue();
+
+        assertEquals(user.getEmail(), savedStudent.getUser().getEmail());
+        assertEquals("Student registered successfully!", result);
+    }
+
+    @Test
+    public void duplicateStudentThrowsException() {
+        User user = new User();
+        user.setEmail((request.getEmail()));
+
+        when(userService.registerUser(any())).thenThrow(
+            new IllegalArgumentException("Email already in use!")
+        );
+
+        Exception e = assertThrows(IllegalArgumentException.class, () -> service.registerStudent(request));
+        assertEquals("Email already in use!", e.getMessage());
+    }
+}
