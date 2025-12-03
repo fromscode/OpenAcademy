067fd326e9f9dde62695db372a06b14aadae2699
Shubham Chakraborty <chakrabortyshubham00@gmail.com>
2025-12-03 06:57:18 +0000

    Create chat related repositories

---
+++ backend_OA/backend_OA/src/main/java/com/openacademy/backend/repository/GroupMemberRepository.java
@@ -0,0 +1,15 @@
+package com.openacademy.backend.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.openacademy.backend.entities.GroupMember;
+
+public interface GroupMemberRepository extends JpaRepository<GroupMember, Long> {
+    boolean existsByGroupIdAndUserId(Long groupId, Long userId);
+
+    List<GroupMember> findByGroupId(Long groupId);
+
+    List<GroupMember> findByUserId(Long userId);
+}
