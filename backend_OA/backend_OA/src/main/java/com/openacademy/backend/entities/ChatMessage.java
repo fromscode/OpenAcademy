2aae68a0cdb1e2de2566032ae1f374f1be0a6eae
Shubham Chakraborty <chakrabortyshubham00@gmail.com>
2025-12-03 06:39:40 +0000

    Create ChatMessage Entity

---
+++ backend_OA/backend_OA/src/main/java/com/openacademy/backend/entities/ChatMessage.java
@@ -0,0 +1,30 @@
+package com.openacademy.backend.entities;
+
+import java.time.Instant;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "messages")
+public class ChatMessage {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    private ChatGroup group;
+
+    @ManyToOne
+    private User sender;
+
+    @Column(columnDefinition = "TEXT")
+    private String content;
+
+    private Instant createdAt = Instant.now();
+}
