d1b4e11146ecc9f01248953f299104becab781f1
Shubham Chakraborty <chakrabortyshubham00@gmail.com>
2026-01-30 13:59:23 +0530

    Modify main.java to create a master admin if not present

--- backend/src/main/java/com/openacademy/backend/BackendApplication.java
+++ backend/src/main/java/com/openacademy/backend/BackendApplication.java
@@ -6,11 +6,23 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import com.openacademy.backend.repository.AdminRepository;
 import com.openacademy.backend.repository.UserRepository;
+
+import jakarta.transaction.Transactional;
+
+import com.openacademy.backend.entities.Admin;
 import com.openacademy.backend.entities.User;
+import com.openacademy.backend.entities.common.Role;
 
 @SpringBootApplication
 public class BackendApplication {
+
+	private final AdminRepository adminRepository;
+
+	BackendApplication(AdminRepository adminRepository) {
+		this.adminRepository = adminRepository;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(BackendApplication.class, args);
@@ -18,9 +30,11 @@ public class BackendApplication {
 	}
 
 	@Bean
+	@Transactional
 	public CommandLineRunner resetAdminPassword(
 			UserRepository repo,
 			PasswordEncoder encoder,
+			AdminRepository adminRepo,
 			// Inject values from properties file
 			@Value("${security.admin.email}") String adminEmail,
 			@Value("${security.admin.password}") String adminPassword) {
@@ -38,7 +52,28 @@ public class BackendApplication {
 				System.out.println("Email: " + adminEmail);
 				System.out.println("------------------------------------------------");
 			} else {
-				System.out.println("!!! ADMIN USER NOT FOUND: " + adminEmail + " !!!");
+				System.out.println("!!! ADMIN USER NOT FOUND, CREATING NEW MASTER ADMIN ");
+				// 1. Create User
+				User user = new User();
+				user.setFirstName("Master");
+				user.setMiddleName("Head");
+				user.setLastName("Admin");
+				user.setEmail(adminEmail);
+				user.setPhoneNumber("master phone number");
+				user.setRole(Role.ADMIN);
+				user.setPassword(encoder.encode(adminPassword)); // Remember to encrypt!
+
+				User savedUser = repo.save(user);
+
+				// 2. Create Admin
+				Admin newAdmin = new Admin();
+				newAdmin.setUser(savedUser);
+
+				adminRepo.save(newAdmin);
+				System.out.println("------------------------------------------------");
+				System.out.println("NEW MASTER ADMIN CREATED");
+				System.out.println("Email: " + adminEmail);
+				System.out.println("------------------------------------------------");
 			}
 		};
 	}
